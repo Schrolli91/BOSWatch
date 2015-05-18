947bc9417cfe54c72fa1cf99450b8a783dbd1106
Schrolli <bastian@dieschrollis.de>
2015-05-18 12:03:42 +0200

    simple logging and data output with plugin

--- plugin_test/plugin_test.py
+++ plugin_test/plugin_test.py
@@ -4,10 +4,23 @@
 import time
 import pluginloader
 
+import logging
+logging.basicConfig(filename='boswatch.log',level=logging.DEBUG, format='%(asctime)s - %(levelname)s: %(message)s', datefmt='%d.%m.%Y %I:%M:%S')
+
+#https://docs.python.org/2/howto/logging.html#logging-basic-tutorial
+#log levels
+#debug - debug messages only for log
+#info - only an information
+#warning
+#error - normal error - program goes further
+#exception - error handler in try:exc: into the message
+#critical - big error, program exit
+
 while True:
     time.sleep(1)
     print ("Alarm!")
+    logging.info("Alarm!")
     for i in pluginloader.getPlugins():
-        print("Loading plugin " + i["name"])
+        logging.debug("Loading plugin " + i["name"])
         plugin = pluginloader.loadPlugin(i)
-        plugin.run()
+        plugin.run("zvei","","","12345","","")
--- plugin_test/plugins/template/__init__.py
+++ plugin_test/plugins/template/__init__.py
@@ -1,2 +1,9 @@
-def run():
-    print("Throw template Plugin")
+import logging
+
+def run(typ,time,frequenz,data1,data2,data3):
+    logging.debug("Throw Template Plugin")
+    try:
+        logging.debug("try 5/0")
+        test = 5/0
+    except:
+        logging.exception("Error in Template Plugin")
