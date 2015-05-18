bd6ae68ea35fa6259f20a65b0a8706a584bf7cbf
Schrolli <bastian@dieschrollis.de>
2015-05-18 12:28:18 +0200

    log all in file and display errors

--- plugin_test/plugin_test.py
+++ plugin_test/plugin_test.py
@@ -4,17 +4,38 @@
 import time
 import pluginloader
 
+#create new logger
 import logging
-logging.basicConfig(filename='boswatch.log', filemode='w', level=logging.DEBUG, format='%(asctime)s - %(levelname)s: %(message)s', datefmt='%d.%m.%Y %I:%M:%S')
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+#set log string format
+formatter = logging.Formatter('%(asctime)s - %(levelname)s: %(message)s', '%d.%m.%Y %I:%M:%S')
+
+#create a file loger
+fh = logging.FileHandler('boswatch.log', 'w')
+fh.setLevel(logging.DEBUG) #log level >= Debug
+fh.setFormatter(formatter)
+logger.addHandler(fh)
+
+#create a display loger
+ch = logging.StreamHandler()
+ch.setLevel(logging.ERROR) #log level >= Error
+ch.setFormatter(formatter)
+logger.addHandler(ch)
 
 #https://docs.python.org/2/howto/logging.html#logging-basic-tutorial
 #log levels
+#----------
 #debug - debug messages only for log
 #info - only an information
 #warning
 #error - normal error - program goes further
 #exception - error handler in try:exc: into the message
 #critical - big error, program exit
+
+
+
 
 while True:
     time.sleep(1)
