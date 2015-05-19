2d64d468a8063bbf9f7804e0c71ad42bcabc6115
Schrolli <bastian@dieschrollis.de>
2015-05-19 15:43:15 +0200

    change Module Names

--- plugin_test/pluginloader.py
+++ plugin_test/pluginloader.py
@@ -7,8 +7,6 @@ import imp
 import os
 
 PluginFolder = "./plugins"
-MainModule = "__init__"
-
 
 def getPlugins():
 	plugins = []
@@ -16,7 +14,7 @@ def getPlugins():
 	for i in possibleplugins:
 		location = os.path.join(PluginFolder, i)
 		# plugins have to be a subdir with MainModule, if not skip
-		if not os.path.isdir(location) or not MainModule + ".py" in os.listdir(location):
+		if not os.path.isdir(location) or not i + ".py" in os.listdir(location):
 			continue
 		logging.debug("found plugin: "+i)
 
@@ -28,10 +26,10 @@ def getPlugins():
 			
 		logging.debug("use Plugin: "+str(usePlugin))
 		if usePlugin:
-			info = imp.find_module(MainModule, [location])
+			info = imp.find_module(i, [location])
 			plugins.append({"name": i, "info": info})
 			logging.debug("append Plugin: "+i)
 	return plugins
 
 def loadPlugin(plugin):
-	return imp.load_module(MainModule, *plugin["info"])
+	return imp.load_module(plugin["name"], *plugin["info"])
