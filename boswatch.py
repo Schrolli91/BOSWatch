1ccc5bb4023de5fef2c12329644d788f0e382166
JHCD <github@jhc.de>
2015-05-24 21:27:50 +0200

    enable pro-working

--- boswatch.py
+++ boswatch.py
@@ -176,7 +176,7 @@ try:
 							#RAW Data from Multimon-NG
 							#ZVEI2: 25832
 							#FMS: 43f314170000 (9=Rotkreuz      3=Bayern 1        Ort 0x25=037FZG 7141Status 3=Einsatz Ab    0=FZG->LST2=III(mit NA,ohneSIGNAL)) CRC correct\n' 
-							#decoded = str(multimon_ng.stdout.readline()) #Get line data from multimon stdout
+							decoded = str(multimon_ng.stdout.readline()) #Get line data from multimon stdout
 							
 							#only for develop
 							#decoded = "ZVEI2: 25832"
