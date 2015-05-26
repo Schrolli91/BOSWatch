be9aa3b7226e902224f3aabbcb697d4a39c99cd1
Schrolli <bastian@dieschrollis.de>
2015-05-26 09:14:04 +0200

    error handling in decoder.py

--- includes/decoder.py
+++ includes/decoder.py
@@ -4,24 +4,27 @@
 import logging
 
 def decode(freq, decoded):
-		
-	#FMS Decoder Section    
-	#check FMS: -> check CRC -> validate -> check double alarm -> log
-	if "FMS:" in decoded:   
-		logging.debug("recieved FMS")
-		from includes.decoders import fms
-		fms.decode(freq, decoded)
+	try:
+		#FMS Decoder Section    
+		#check FMS: -> check CRC -> validate -> check double alarm -> log
+		if "FMS:" in decoded:   
+			logging.debug("recieved FMS")
+			from includes.decoders import fms
+			fms.decode(freq, decoded)
 
-	#ZVEI Decoder Section
-	#check ZVEI: -> validate -> check double alarm -> log     
-	if "ZVEI2:" in decoded:
-		logging.debug("recieved ZVEI")			
-		from includes.decoders import zvei
-		zvei.decode(freq, decoded)
-		
-	#POCSAG Decoder Section
-	#check POCSAG -> validate -> check double alarm -> log      
-	if "POCSAG" in decoded:
-		logging.debug("recieved POCSAG")				
-		from includes.decoders import poc
-		poc.decode(freq, decoded)
+		#ZVEI Decoder Section
+		#check ZVEI: -> validate -> check double alarm -> log     
+		if "ZVEI2:" in decoded:
+			logging.debug("recieved ZVEI")			
+			from includes.decoders import zvei
+			zvei.decode(freq, decoded)
+			
+		#POCSAG Decoder Section
+		#check POCSAG -> validate -> check double alarm -> log      
+		if "POCSAG" in decoded:
+			logging.debug("recieved POCSAG")				
+			from includes.decoders import poc
+			poc.decode(freq, decoded)
+			
+	except:
+		logging.exception("cannot start decoder")
