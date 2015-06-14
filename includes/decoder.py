16865b2c57d162df9ac994d6b8a3fca49513ee56
JHCD <github@jhc.de>
2015-06-14 19:50:13 +0200

    bug with "enabled demodulators"-line of multimon-ng output, when using pocsag - to enable detecting pocsag-bitrate the first searching is only on "*pocsag*" - in the "enabled demodulators"-line every given demodulator is displayed, even "POCSAG*" - pocsag-bitrate detection is searching for "POCSAG{512|1024|2400}:" -> now the "enabled demodulators"-line will be ignored

--- includes/decoder.py
+++ includes/decoder.py
@@ -24,23 +24,27 @@ def decode(freq, decoded):
 	@exception: Exception if decoder File call failed
 	"""
 	try:
-		#FMS Decoder Section    
-		#check FMS: -> check CRC -> validate -> check double alarm -> log
+		# FMS Decoder Section    
+		# check FMS: -> check CRC -> validate -> check double alarm -> log
 		if "FMS:" in decoded:   
 			logging.debug("recieved FMS")
 			from includes.decoders import fms
 			fms.decode(freq, decoded)
 
-		#ZVEI Decoder Section
-		#check ZVEI: -> validate -> check double alarm -> log     
-		if "ZVEI2:" in decoded:
+		# ZVEI Decoder Section
+		# check ZVEI: -> validate -> check double alarm -> log     
+		elif "ZVEI2:" in decoded:
 			logging.debug("recieved ZVEI")			
 			from includes.decoders import zvei
 			zvei.decode(freq, decoded)
 			
-		#POCSAG Decoder Section
-		#check POCSAG -> validate -> check double alarm -> log      
-		if "POCSAG" in decoded:
+		# For POCSAG we have to ignore the first multimon-ng line
+		elif "Enabled demodulators:" in decoded:
+			pass
+			
+		# POCSAG Decoder Section
+		# check POCSAG -> validate -> check double alarm -> log      
+		elif "POCSAG" in decoded:
 			logging.debug("recieved POCSAG")				
 			from includes.decoders import poc
 			poc.decode(freq, decoded)
