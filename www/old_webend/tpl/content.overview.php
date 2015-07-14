bfd4e2c1a4ed171ec93fff1676bb8875a4ce786a
Schrolli <bastian@dieschrollis.de>
2015-07-14 07:16:56 +0200

    move old and new webend in separate folders #62

---
+++ www/old_webend/tpl/content.overview.php
@@ -0,0 +1,27 @@
+<?php
+	//read FMS
+	$db->query("SELECT id, time, fms, status, direction, tsi FROM ".$tableFMS." ORDER BY id DESC LIMIT 50");
+	$Rows = array();
+	while ($daten = $db->fetchAssoc())
+	{
+		$Rows[] = $daten;
+	}
+	$tpl['fms'] = $Rows;
+
+	//read ZVEI
+	$db->query("SELECT id, time, zvei FROM ".$tableZVEI." ORDER BY id DESC LIMIT 50");
+	$Rows = array();
+	while ($daten = $db->fetchAssoc())
+	{
+		$Rows[] = $daten;
+	}
+	$tpl['zvei'] = $Rows;
+			//read POCSAG
+	$db->query("SELECT id, time, ric, funktion, text FROM ".$tablePOC." ORDER BY id DESC LIMIT 50");
+	$Rows = array();
+	while ($daten = $db->fetchAssoc())
+	{
+		$Rows[] = $daten;
+	}
+	$tpl['poc'] = $Rows;
+?>
