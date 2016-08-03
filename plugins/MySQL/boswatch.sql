faf74364f62fe599a1eb214f4fe408620cd652ca
Cryjack <Cryjack@users.noreply.github.com>
2016-08-03 10:23:01 +0200

    Update boswatch.sql

--- plugins/MySQL/boswatch.sql
+++ plugins/MySQL/boswatch.sql
@@ -19,9 +19,23 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
 
+-- --------------------------------------------------------
+
 --
--- Datenbank: `boswatch`
+-- Datenbank anlegen `boswatch`
 --
+
+CREATE DATABASE IF NOT EXISTS boswatch;
+USE boswatch; 
+
+-- --------------------------------------------------------
+
+--
+-- Benutzer erstellen für Datenbank `boswatch`
+--
+
+GRANT ALL ON * to 'boswatch'@'localhost' identified by 'boswatch';
+FLUSH PRIVILEGES;
 
 -- --------------------------------------------------------
 
@@ -72,6 +86,74 @@ CREATE TABLE IF NOT EXISTS `bos_zvei` (
   `description` text,
   PRIMARY KEY (`ID`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `bos_signal`
+--
+
+CREATE TABLE IF NOT EXISTS `bos_signal` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `time` DATETIME NOT NULL,
+    `ric` VARCHAR(7) NOT NULL DEFAULT '0',
+    PRIMARY KEY (`ID`)
+)  ENGINE=MYISAM DEFAULT CHARSET=UTF8 AUTO_INCREMENT=1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `bos_weblogin`
+--
+
+CREATE TABLE IF NOT EXISTS `bos_weblogin` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `user` VARCHAR(150) DEFAULT NULL,
+    `password` VARCHAR(32) DEFAULT NULL,
+    `isadmin` BOOLEAN DEFAULT FALSE,
+    PRIMARY KEY (`id`)
+)  ENGINE=MYISAM DEFAULT CHARSET=UTF8 AUTO_INCREMENT=1;
+
+INSERT INTO `bos_weblogin` (`id`, `user`, `password`, `isadmin`) VALUES (NULL, 'admin', '21232f297a57a5a743894a0e4a801fc3', '1');
+
+-- --------------------------------------------------------
+
+--
+-- Schedule für Tabelle `bos_pocsag`
+--
+CREATE EVENT IF NOT EXISTS `Delete POCSAG Entries > 3 Months` 
+	ON SCHEDULE EVERY 1 DAY 
+    STARTS '2016-01-01 00:00:00' 
+    ON COMPLETION PRESERVE ENABLE 
+    DO 
+		DELETE FROM bos_pocsag WHERE time < DATE_SUB(NOW(),INTERVAL 3 MONTH);
+		
+
+-- --------------------------------------------------------
+
+--
+-- Schedule für Tabelle `bos_fms`
+--
+
+CREATE EVENT IF NOT EXISTS `Delete FMS Entries > 3 Months` 
+	ON SCHEDULE EVERY 1 DAY 
+    STARTS '2016-01-01 00:00:00' 
+    ON COMPLETION PRESERVE ENABLE 
+    DO 
+		DELETE FROM bos_fms WHERE time < DATE_SUB(NOW(),INTERVAL 3 MONTH);
+		
+-- --------------------------------------------------------
+
+--
+-- Schedule für Tabelle `bos_zvei`
+--
+        
+CREATE EVENT IF NOT EXISTS `Delete ZVEI Entries > 3 Months` 
+	ON SCHEDULE EVERY 1 DAY 
+    STARTS '2016-01-01 00:00:00' 
+    ON COMPLETION PRESERVE ENABLE 
+    DO 
+		DELETE FROM bos_zvei WHERE time < DATE_SUB(NOW(),INTERVAL 3 MONTH); 
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
