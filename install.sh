6ed77c9945899adb26b80c581647c65218edab83
Bastian Schroll <bastian@dieschrollis.de>
2016-11-20 13:44:53 +0100

    set new install file as standard

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 tput clear
 tput civis
 echo "     ____  ____  ______       __      __       __   "
@@ -9,92 +9,128 @@ echo " /_____/\____//____/ |__/|__/\__,_/\__/\___/_/ /_/  "
 echo "            German BOS Information Script           "
 echo "                 by Bastian Schroll                 "
 echo ""
-echo "This may take several minutes... Don't panic!"
+echo "This may take a several minutes... Don't panic!"
 echo ""
-echo "Caution, the script doesn't install a Webserver with PHP and MySQL"
-echo "so you have to install it later manually if you want to use the MySQL features"
+echo "Caution, script don't install a Webserver with PHP and MySQL"
+echo "So you have to make up manually if you want to use MySQL support"
 
-mkdir -p ~/boswatch/install
+boswatchpath=/opt/boswatch
+mkdir -p $boswatchpath
+
+for (( i=1; i<=$#; i=$i+2 )); do
+    t=$((i + 1))
+    eval arg=\$$i
+    eval arg2=\$$t
+
+    case $arg in
+      -r|--reboot)
+        case $arg2 in
+          y|yes) reboot=true ;;
+          n|no) reboot=false ;;
+          *) echo "Please use y/yes or n/no for reboot" ; exit 1 ;;
+        esac ;;
+
+      -b|--branch)
+      case $arg2 in
+        dev) echo "       !!! WARNING: you are using the DEV BRANCH !!!      "; branch=dev ;;
+        *) branch=master ;;
+      esac ;;
+
+      *) echo "Internal error!" ; exit 1 ;;
+    esac
+done
+
+mkdir -p $boswatchpath/install
+
+echo ""
 
 tput cup 13 15
 echo "[ 1/10] [#---------]"
 tput cup 15 5
-echo "-> refreshing package versions............."
-apt-get update -y > ~/boswatch/install/setup_log.txt 2>&1
+echo "-> make a apt-get update................"
+apt-get update -y > $boswatchpath/install/setup_log.txt 2>&1
 
 tput cup 13 15
 echo "[ 2/10] [##--------]"
 tput cup 15 5
-echo "-> downloading GIT and other stuff.........."
-apt-get -y install git cmake build-essential libusb-1.0 qt4-qmake libpulse-dev libx11-dev sox >> ~/boswatch/install/setup_log.txt 2>&1
+echo "-> download GIT an other stuff.........."
+apt-get -y install git cmake build-essential libusb-1.0 qt4-qmake qt4-default qt5-default libpulse-dev libx11-dev sox >> $boswatchpath/install/setup_log.txt 2>&1
 
 tput cup 13 15
 echo "[ 3/10] [###-------]"
 tput cup 15 5
-echo "-> downloading rtl_fm......................"
-cd ~/boswatch/install
-git clone git://git.osmocom.org/rtl-sdr.git >> ~/boswatch/install/setup_log.txt 2>&1
+echo "-> download rtl_fm......................"
+cd $boswatchpath/install
+git clone git://git.osmocom.org/rtl-sdr.git >> $boswatchpath/install/setup_log.txt 2>&1
 cd rtl-sdr/
 
 tput cup 13 15
 echo "[ 4/10] [####------]"
 tput cup 15 5
-echo "-> compiling rtl_fm........................"
+echo "-> compile rtl_fm......................"
 mkdir -p build && cd build
-cmake ../ -DINSTALL_UDEV_RULES=ON >> ~/boswatch/install/setup_log.txt 2>&1
-make >> ~/boswatch/install/setup_log.txt 2>&1
-make install >> ~/boswatch/install/setup_log.txt 2>&1
-ldconfig >> ~/boswatch/install/setup_log.txt 2>&1
+cmake ../ -DINSTALL_UDEV_RULES=ON >> $boswatchpath/install/setup_log.txt 2>&1
+make >> $boswatchpath/install/setup_log.txt 2>&1
+make install >> $boswatchpath/install/setup_log.txt 2>&1
+ldconfig >> $boswatchpath/install/setup_log.txt 2>&1
 
 tput cup 13 15
 echo "[ 5/10] [#####-----]"
 tput cup 15 5
-echo "-> downloading multimon-ng................"
-cd ~/boswatch/install
-git clone https://github.com/EliasOenal/multimonNG.git >> ~/boswatch/install/setup_log.txt 2>&1
-cd multimonNG/
+echo "-> download multimon-ng................"
+cd $boswatchpath/install
+git clone https://github.com/EliasOenal/multimonNG.git >> $boswatchpath/install/setup_log.txt 2>&1
+cd $boswatchpath/install/multimonNG/
 
 tput cup 13 15
 echo "[ 6/10] [######----]"
 tput cup 15 5
-echo "-> compiling multimon-ng................."
+echo "-> compile multimon-ng................."
 mkdir -p build
 cd build
-qmake ../multimon-ng.pro >> ~/boswatch/install/setup_log.txt 2>&1
-make >> ~/boswatch/install/setup_log.txt 2>&1
-make install >> ~/boswatch/install/setup_log.txt 2>&1
+qmake ../multimon-ng.pro >> $boswatchpath/install/setup_log.txt 2>&1
+make >> $boswatchpath/install/setup_log.txt 2>&1
+make install >> $boswatchpath/install/setup_log.txt 2>&1
 
 tput cup 13 15
 echo "[ 7/10] [#######---]"
 tput cup 15 5
-echo "-> downloading the MySQL Connector for Python."
-cd ~/boswatch/install
-wget "http://dev.mysql.com/get/Downloads/Connector-Python/mysql-connector-python-1.0.9.tar.gz/from/http://cdn.mysql.com/" -O mysql-connector.tar >> ~/boswatch/install/setup_log.txt 2>&1
-tar xfv mysql-connector.tar >> ~/boswatch/install/setup_log.txt 2>&1
-cd mysql-connector-python*
+echo "-> download MySQL Connector for Python."
+cd $boswatchpath/install
+wget "http://dev.mysql.com/get/Downloads/Connector-Python/mysql-connector-python-1.0.9.tar.gz/from/http://cdn.mysql.com/" -O mysql-connector.tar >> $boswatchpath/install/setup_log.txt 2>&1
+tar xfv mysql-connector.tar >> $boswatchpath/install/setup_log.txt 2>&1
+cd $boswatchpath/install/mysql-connector-python*
 
 tput cup 13 15
 echo "[ 8/10] [########--]"
 tput cup 15 5
-echo "-> installing the MySQL Connector for Python.."
+echo "-> install MySQL Connector for Python.."
 chmod +x ./setup.py
-./setup.py install >> ~/boswatch/install/setup_log.txt 2>&1
+./setup.py install >> $boswatchpath/install/setup_log.txt 2>&1
 
 tput cup 13 15
 echo "[ 9/10] [#########-]"
 tput cup 15 5
-echo "-> downloading BOSWatch from GitHub.........."
-cd ~/boswatch
-git clone https://github.com/Schrolli91/BOSWatch >> ~/boswatch/install/setup_log.txt 2>&1
+echo "-> download BOSWatch..................."
+cd $boswatchpath/
+
+case $branch in
+  dev) git clone -b develop https://github.com/Schrolli91/BOSWatch >> $boswatchpath/install/setup_log.txt 2>&1 ;;
+  *) git clone -b master https://github.com/Schrolli91/BOSWatch >> $boswatchpath/install/setup_log.txt 2>&1 ;;
+esac
 
 tput cup 13 15
 echo "[10/10] [##########]"
 tput cup 15 5
-echo "-> Configuring BOSWatch................"
-cd ~/boswatch
+echo "-> configure..........................."
+cd $boswatchpath/
 chmod +x *
 echo "# BOSWatch - blacklist the DVB drivers to avoid conflict with the SDR driver\n blacklist dvb_usb_rtl28xxu \n blacklist rtl2830\n blacklist dvb_usb_v2\n blacklist dvb_core" >> /etc/modprobe.d/boswatch_blacklist_sdr.conf
 
 tput cup 17 1
-echo "BOSWatch is now installed in ~/boswatch/"
-echo "Installation finished!"
+echo "BOSWatch are now installed in $boswatchpath/"
+echo "Install ready!"
+
+if [ $reboot == "true" ]; then
+  /sbin/reboot
+fi
