08c671cd0992b900d7872d9b19dfe2cdfefbf956
Florian <web@florian-thillmann.de>
2017-04-16 23:48:46 +0200

    Update Sms77.py
    Adapting to new API

--- plugins/Sms77/Sms77.py
+++ plugins/Sms77/Sms77.py
@@ -80,8 +80,8 @@ def run(typ,freq,data):
 				#
 				logging.debug("send Sms77 %s", typ)
 
-				conn = httplib.HTTPSConnection("gateway.sms77.de:443")
-				conn.request("POST", "",
+				cconn = httplib.HTTPSConnection("gateway.sms77.io",443)
+				conn.request("POST", "/api/sms",
 				urllib.urlencode({
 					"u": globalVars.config.get("Sms77", "user"),
 					"p": globalVars.config.get("Sms77", "password"),
