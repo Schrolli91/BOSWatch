b5d860f5cbdf010bc5f0f99485e3ac2bb9a7b4c5
Philip Giuliani <giiuliio@gmail.com>
2017-10-09 14:11:26 +0200

    Fix output paths of the install script

--- install.sh
+++ install.sh
@@ -213,8 +213,8 @@ echo "Installation ready!"
 tput cup 19 3
 echo "Watch out: to run BOSWatch you have to generate and modify the config.ini!"
 echo "Do the following steps to have a running version of BOSWatch:"
-echo "sudo cp $boswatchpath/BOSWatch/config/config.template.ini $boswatchpath/BOSWatch/config/config.ini"
-echo "sudo nano $boswatchpath/BOSWatch/config/config.ini"
+echo "sudo cp $boswatchpath/config/config.template.ini $boswatchpath/config/config.ini"
+echo "sudo nano $boswatchpath/config/config.ini"
 echo "and modify the config as you need. This step is optional if you are upgrading an old version of BOSWatch. "
 
 tput cnorm
