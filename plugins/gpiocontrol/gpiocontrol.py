4557f3399550de181222d5c7ef6aec0bbce150b9
mrduckspace <34840030+mrduckspace@users.noreply.github.com>
2019-11-21 13:16:05 +0100

    Update gpiocontrol.py

--- plugins/gpiocontrol/gpiocontrol.py
+++ plugins/gpiocontrol/gpiocontrol.py
@@ -36,7 +36,7 @@ def onLoad():
 	GPIO.setmode(GPIO.BCM)
 	GPIO.setwarnings(False)
 	GPIO.setup(GPIOPIN, GPIO.OUT)
-  GPIO.output(GPIOPIN, GPIO.HIGH)
+  	GPIO.output(GPIOPIN, GPIO.HIGH)
 
 	return
 
@@ -51,7 +51,7 @@ def run(typ,freq,data):
 
 			logging.debug(globalVars.config.get("gpiocontrol", "pin"))
 			logging.debug(globalVars.config.get("gpiocontrol", "triggertime"))
-      logging.debug(globalVars.config.get("gpiocontrol", "activerics"))
+   			logging.debug(globalVars.config.get("gpiocontrol", "activerics"))
             
 			########## User Plugin CODE ##########
 			if typ == "FMS":
@@ -68,8 +68,8 @@ def run(typ,freq,data):
                         th.start()
                     else:
                         logging.info("Ric not in activerics")
-			else:
-				logging.warning("Invalid Typ: %s", typ)
+		else:
+                    logging.warning("Invalid Typ: %s", typ)
 			########## User Plugin CODE ##########
 
 	except:
@@ -80,7 +80,7 @@ def trigger():
 	GPIO.output(GPIOPIN, GPIO.LOW)
 	logging.info("GPIOPIN %s on", GPIOPIN)
 	time.sleep(waitTime)
-  GPIO.output(GPIOPIN, GPIO.HIGH)
+  	GPIO.output(GPIOPIN, GPIO.HIGH)
 	logging.info("GPIOPIN %s off", GPIOPIN)
 
 	return
