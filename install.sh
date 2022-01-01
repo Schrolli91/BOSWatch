92f00ef36c86c088d6e1ad52df3cb1801c534c0e
Julian Wöhrer <julian.woehrer@gmail.com>
2022-01-01 22:50:18 +0100

    install skript for install python3 and the qt4

--- install.sh
+++ install.sh
@@ -108,7 +108,8 @@ tput cup 13 15
 echo "[ 2/9] [##-------]"
 tput cup 15 5
 echo "-> download GIT and other stuff.........."
-apt-get -y install git cmake build-essential libusb-1.0 qt4-qmake qt4-default libpulse-dev libx11-dev sox python-pip >> $boswatch_install_path/setup_log.txt 2>&1
+sudo add-apt-repository ppa:rock-core/qt4 -y >> $boswatch_install_path/setup_log.txt 2>&1
+apt-get -y install git cmake build-essential libusb-1.0 qt4-qmake qt4-default libpulse-dev libx11-dev sox python3-pip >> $boswatch_install_path/setup_log.txt 2>&1
 exitcodefunction $? download stuff
 
 # download BOSWatch via git
