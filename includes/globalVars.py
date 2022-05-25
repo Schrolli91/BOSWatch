66b5ddf4675c2f12322e3a3237f5e48dc77c6a50
Bastian Schroll <info@schroll-it.de>
2022-05-25 05:57:38 +0000

    fix version string

--- includes/globalVars.py
+++ includes/globalVars.py
@@ -9,9 +9,9 @@ Global variables
 """
 
 # version info
-versionNr = "2.5.2"
-branch = "master"
-buildDate = "08.01.2021"
+versionNr = "2.5.3"
+branch = "dev"
+buildDate = "unreleased"
 
 # Global variables
 config = 0
